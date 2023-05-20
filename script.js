e50cab8d17f8e139b873b3a1d8c89e775f22d3ae
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-20 15:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const prices = document.querySelectorAll('[data-ns-test=price]');
+let totalPrice = 0;
+
+prices.forEach((price) => {
+
+  totalPrice += parseFloat(price.innerText);
+});
+
+const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotalCell.innerText = totalPrice.toFixed(2);
